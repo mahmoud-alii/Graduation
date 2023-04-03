867688d1bb0fe0249d711e04a31c7ee4265e1ad8
mahmoud-alii <mahmoudaliisaad@gmail.com>
2023-04-03 13:01:13 +0200

    Added RegisterAttendance

--- Controllers/login.cs
+++ Controllers/login.cs
@@ -20,15 +20,20 @@ namespace GradProject.Controllers
             private bool IsDoctor { get; set; }
         }
 
+        public class Student : Person
+        {
+
+        }
+
         [HttpPost]
-        public IActionResult Authenticate([FromBody] Person login)
+        public IActionResult Authenticate([FromBody] Person person)
         {
             int type = 0;
 
             MySqlConnection cnn;
             String trial = @"server=127.0.0.1;database=aast;userid=root;password=;";
             cnn = new MySqlConnection(trial);
-            string query = $"SELECT type FROM aast_person WHERE id='{login.Id}' AND pass='{login.Pass}'";
+            string query = $"SELECT type FROM aast_person WHERE id='{person.Id}' AND pass='{person.Pass}'";
             MySqlCommand command = new MySqlCommand(query, cnn);
 
             try
@@ -54,6 +59,42 @@ namespace GradProject.Controllers
             cnn.Close();
 
             return Ok(new { AccountType = type });
+        }
+
+        [HttpGet]
+        public IActionResult RegisterAttendance([FromBody] Person person)
+        {
+            String pass = "";
+
+            MySqlConnection cnn;
+            String trial = @"server=127.0.0.1;database=aast;userid=root;password=;";
+            cnn = new MySqlConnection(trial);
+            string query = $"SELECT pass FROM aast_person WHERE id='{person.Id}' AND type='1'";
+            MySqlCommand command = new MySqlCommand(query, cnn);
+
+            try
+            {
+                cnn.Open();
+                MySqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        pass = reader.GetString(0);
+                    }
+                }
+                reader.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error" + e.Message);
+            }
+            
+            cnn.Close();
+
+            return Ok(new { AttendancePass = pass });
         }
     }
 }
