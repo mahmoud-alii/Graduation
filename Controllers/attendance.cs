cfb6ece5478f1da5bf423e608d28f4c5f7a47c0b
OmarMKM <127153224+OmarMKM@users.noreply.github.com>
2023-04-08 01:26:07 +0200

    Added GetCourses

--- Controllers/attendance.cs
+++ Controllers/attendance.cs
@@ -17,6 +17,53 @@ namespace Graduation.Controllers
             public string slots { get; set; }
             public string class_no { get; set; }
         }
+        public class Teaches
+        {
+            public int instructor_id { get; set; }
+            public string course_code { get; set; }
+            public string class_no { get; set; }
+
+        }
+
+        [HttpPost]
+        public IActionResult GetCourses([FromBody] Teaches teaches)
+        {
+            string[] courses = null ;
+            int i =0;
+            MySqlConnection cnn;
+            String trial = @"server=127.0.0.1;database=attendance;userid=root;password=;";
+            cnn = new MySqlConnection(trial);
+            string query = $"SELECT course_code FROM teaches WHERE instructor_id='{teaches.instructor_id}'";
+            MySqlCommand command = new MySqlCommand(query, cnn);
+
+            try
+            {
+                cnn.Open();
+                MySqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        courses[i] = reader.GetString(i);
+                        i++;
+                    }
+                }
+                reader.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error" + e.Message);
+            }
+
+            cnn.Close();
+
+            return Ok(new
+            {
+                Courses= courses , NumberOfCourses=i
+            });
+        }
 
         [HttpPost]
         public IActionResult GetClassNo([FromBody] Schedules schedules)
@@ -59,39 +106,39 @@ namespace Graduation.Controllers
         }
 
 
-        [HttpPost]
-        public IActionResult RegisterAttendance([FromBody] Student student)
-        {
-            String pass = "";
-            MySqlConnection cnn;
-            String trial = @"server=127.0.0.1;database=attendance;userid=root;password=;";
-            cnn = new MySqlConnection(trial);
-            string query = $"SELECT pass FROM person WHERE id='{student.Id}' AND type='1'";
-            MySqlCommand command = new MySqlCommand(query, cnn);
+        //[HttpPost]
+        //public IActionResult RegisterAttendance([FromBody] Student student)
+        //{
+        //    String pass = "";
+        //    MySqlConnection cnn;
+        //    String trial = @"server=127.0.0.1;database=attendance;userid=root;password=;";
+        //    cnn = new MySqlConnection(trial);
+        //    string query = $"SELECT pass FROM person WHERE id='{student.Id}' AND type='1'";
+        //    MySqlCommand command = new MySqlCommand(query, cnn);
 
-            try
-            {
-                cnn.Open();
-                MySqlDataReader reader = command.ExecuteReader();
+        //    try
+        //    {
+        //        cnn.Open();
+        //        MySqlDataReader reader = command.ExecuteReader();
 
-                if (reader.HasRows)
-                {
-                    while (reader.Read())
-                    {
-                        pass = reader.GetString(0);
-                    }
-                }
-                reader.Close();
+        //        if (reader.HasRows)
+        //        {
+        //            while (reader.Read())
+        //            {
+        //                pass = reader.GetString(0);
+        //            }
+        //        }
+        //        reader.Close();
 
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error" + e.Message);
-            }
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine("Error" + e.Message);
+        //    }
 
-            cnn.Close();
+        //    cnn.Close();
 
-            return Ok(new { AttendancePass = pass });
-        }
+        //    return Ok(new { AttendancePass = pass });
+        //}
     }
 }
