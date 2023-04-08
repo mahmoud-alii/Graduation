3856c5d44168bfc82c64cd73b70555984bde4a63
OmarMKM <127153224+OmarMKM@users.noreply.github.com>
2023-04-08 03:08:45 +0200

    Added AuthenticateStudent_Course
    zwdt brdo f table takes w hb3tlk el datab ase el gdeda fl discord

--- Controllers/attendance.cs
+++ Controllers/attendance.cs
@@ -31,15 +31,21 @@ namespace Graduation.Controllers
             public int student_id { get; set; }
 
         }
+        public class Takes
+        {
+            public int student_id { get; set; }
+            public string course_code { get; set; }
+            public string class_no { get; set; }
+
+        }
 
         [HttpPost]
-        public IActionResult GetStudentID([FromBody] Nfc_serial nfc_Serial, int student_id)
+        public IActionResult AuthenticateStudent_Course([FromBody] Takes takes, bool x)
         {
-            
             MySqlConnection cnn;
             String trial = @"server=127.0.0.1;database=attendance;userid=root;password=;";
             cnn = new MySqlConnection(trial);
-            string query = $"SELECT student_id FROM nfc_serial WHERE serial_no='{nfc_Serial.serial_no}'";
+            string query = $"SELECT CASE WHEN EXISTS (SELECT * FROM takes WHERE student_id= '{takes.student_id}' AND course_code = '{takes.course_code}' AND class_no = '{takes.class_no}') THEN 'TRUE' ELSE 'FALSE' END ";
             MySqlCommand command = new MySqlCommand(query, cnn);
 
             try
@@ -51,7 +57,7 @@ namespace Graduation.Controllers
                 {
                     while (reader.Read())
                     {
-                        student_id = reader.GetInt32(0);
+                        x = reader.GetBoolean(0);
                     }
                 }
                 reader.Close();
@@ -66,9 +72,47 @@ namespace Graduation.Controllers
 
             return Ok(new
             {
-                StudentID = student_id
+                isFound = x
             });
         }
+
+        //[HttpPost]
+        //public IActionResult GetStudentID([FromBody] Nfc_serial nfc_Serial, int student_id)
+        //{
+
+        //    MySqlConnection cnn;
+        //    String trial = @"server=127.0.0.1;database=attendance;userid=root;password=;";
+        //    cnn = new MySqlConnection(trial);
+        //    string query = $"SELECT student_id FROM nfc_serial WHERE serial_no='{nfc_Serial.serial_no}'";
+        //    MySqlCommand command = new MySqlCommand(query, cnn);
+
+        //    try
+        //    {
+        //        cnn.Open();
+        //        MySqlDataReader reader = command.ExecuteReader();
+
+        //        if (reader.HasRows)
+        //        {
+        //            while (reader.Read())
+        //            {
+        //                student_id = reader.GetInt32(0);
+        //            }
+        //        }
+        //        reader.Close();
+
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine("Error" + e.Message);
+        //    }
+
+        //    cnn.Close();
+
+        //    return Ok(new
+        //    {
+        //        StudentID = student_id
+        //    });
+        //}
 
 
         //[HttpPost]
