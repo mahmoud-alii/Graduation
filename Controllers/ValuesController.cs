7d9988aabbcdb1c12f905ea0fbb20a7d24bb4bc7
mahmoud-alii <mahmoudaliisaad@gmail.com>
2023-04-27 19:55:45 +0200

    Update ValuesController.cs

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using static Graduation.Controllers.Login;
+using MySql.Data.MySqlClient;
 
 namespace Graduation.Controllers
 {
@@ -7,5 +9,39 @@ namespace Graduation.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        [HttpPost]
+        public IActionResult RegisterAttendance([FromBody] Person person)
+        {
+            String pass = "";
+            MySqlConnection cnn;
+            String trial = @"server=127.0.0.1;database=attendance;userid=root;password=;";
+            cnn = new MySqlConnection(trial);
+            string query = $"SELECT pass FROM person WHERE id='{person.Id}' AND type='{person.Type}'";
+            MySqlCommand command = new MySqlCommand(query, cnn);
+
+            try
+            {
+                cnn.Open();
+                MySqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        pass = reader.GetString(0);
+                    }
+                }
+                reader.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error" + e.Message);
+            }
+
+            cnn.Close();
+
+            return Ok(new { AttendancePass = pass });
+        }
     }
 }
