f8260a0e5e49580f0ef4453e61cb48e266d6c9d9
mahmoud-alii <mahmoudaliisaad@gmail.com>
2023-04-27 21:51:12 +0200

    Added ExcelController
    Added the ExcelController and required extensions.

---
+++ Controllers/ExcelController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using OfficeOpenXml;
+using System.IO;
+
+namespace YourNamespace.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ExcelController : ControllerBase
+    {
+        [HttpGet]
+        public FileContentResult Get()
+        {
+            var connectionString = @"server=127.0.0.1;database=attendance;userid=root;password=;";
+            var fileName = "attendance.xlsx";
+
+            using (var package = new ExcelPackage())
+            {
+                var worksheet = package.Workbook.Worksheets.Add("Sheet1");
+
+                using (var connection = new MySqlConnection(connectionString))
+                {
+                    var query = "SELECT * FROM person";
+                    var command = new MySqlCommand(query, connection);
+
+                    connection.Open();
+                    var reader = command.ExecuteReader();
+
+                    var row = 2;
+                    worksheet.Cells[1, 1].Value = "ID";
+                    worksheet.Cells[1, 2].Value = "Password";
+                    while (reader.Read())
+                    {
+                        worksheet.Cells[row, 1].Value = reader.GetString(0);
+                        worksheet.Cells[row, 2].Value = reader.GetString(1);
+                        // add more columns as needed
+                        row++;
+                    }
+
+                    reader.Close();
+                    connection.Close();
+                }
+
+                var fileBytes = package.GetAsByteArray();
+                return File(fileBytes, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
+            }
+        }
+    }
+}
