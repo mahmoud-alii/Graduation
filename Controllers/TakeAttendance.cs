b0200e57c35a3bbf5273b03049d53e40a44b9272
OmarMKM <127153224+OmarMKM@users.noreply.github.com>
2023-06-05 16:08:38 +0300

    Revert "Merge branch 'master' of https://github.com/mahmoud-alii/Graduation"
    This reverts commit edc44c4f568b296f8e1ddcb2bc644c8b07b1816f, reversing
    changes made to e1cfa35da25148d8c4222e4cba905d6552b8e6d5.

--- Controllers/TakeAttendance.cs
+++ Controllers/TakeAttendance.cs
@@ -14,12 +14,11 @@ namespace Graduation.Controllers
         public IActionResult takeAttendance([FromBody] Attendance attendance)
         {
             Boolean x = true;
-            var message = "";
             MySqlConnection cnn;
-            //String trial = @"server=127.0.0.1;database=attendance;userid=root;password=;";
-            String trial = @"server=aast-db.cf4afzenuusl.us-east-1.rds.amazonaws.com;database=attendance;userid=ahmed_admin;password=777888999;";
+            String trial = @"server=127.0.0.1;database=attendance;userid=root;password=;";
+            //String trial = @"server=aast-db.cf4afzenuusl.us-east-1.rds.amazonaws.com;database=attendance;userid=ahmed_admin;password=777888999;";
             cnn = new MySqlConnection(trial);
-            string query = $"SELECT CASE WHEN EXISTS (SELECT * FROM attendance WHERE student_id= '{attendance.student_id}' AND schedule_id = '{attendance.schedule_id}' AND week_number = '{attendance.week_no}') THEN 'TRUE' ELSE 'FALSE' END ";
+            string query = $"SELECT CASE WHEN EXISTS (SELECT * FROM attendance1 WHERE student_id= '{attendance.student_id}' AND schedule_id = '{attendance.schedule_id}' AND week_number = '{attendance.week_no}') THEN 'TRUE' ELSE 'FALSE' END ";
             MySqlCommand command = new MySqlCommand(query, cnn);
             try
             {
@@ -41,54 +40,24 @@ namespace Graduation.Controllers
             }
 
             if (x == false) {
-                string query2 = $"INSERT INTO attendance (student_id, schedule_id, week_number, attended) VALUES ('{attendance.student_id}', '{attendance.schedule_id}', '{attendance.week_no}','{1}')";
+                string sql = $"INSERT INTO attendance1 (student_id, schedule_id, week_number, attended) VALUES ('{attendance.student_id}', '{attendance.schedule_id}', '{attendance.week_no}','{1}')";
                 //string query2 = $"INSERT INTO attendance1 (student_id,schedule_id,week_number,attended) VALUES (@Value1, @Value2 , @Value3, @Value4 )";
-                MySqlCommand cmd = new MySqlCommand(query2, cnn);
+                MySqlCommand cmd = new MySqlCommand(sql, cnn);
                 //cmd.Parameters.AddWithValue("@Value1", attendance.student_id);
                 //cmd.Parameters.AddWithValue("@Value2", attendance.schedule_id);
                 //cmd.Parameters.AddWithValue("@Value3", attendance.week_no);
                 //cmd.Parameters.AddWithValue("@Value4", 1);
                 cmd.ExecuteNonQuery();
                 cnn.Close();
-                message = "Attendance taken";
-                return Ok(new { message = message });
+                var message = new { message = "Attendance taken" };
+                return Ok(message);
             }
             else
             {
-                int a = 2;
-                string query3 = $"SELECT attended FROM attendance WHERE student_id = '{attendance.student_id}' AND schedule_id = '{attendance.schedule_id}' AND week_number = '{attendance.week_no}'";
-                MySqlCommand cmnd = new MySqlCommand(query3, cnn);
-                try
-                {
-                    cnn.Open();
-                    MySqlDataReader reader = cmnd.ExecuteReader();
-
-                    if (reader.HasRows)
-                    {
-                        while (reader.Read())
-                        {
-                            a = reader.GetInt32(0);
-                        }
-                    }
-                    reader.Close();
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Error" + e.Message);
-                }
-
-                if(a == 0) {
-                    string query4 = $"UPDATE attendance SET attended = '{1}' WHERE student_id= '{attendance.student_id}' AND schedule_id = '{attendance.schedule_id}' AND week_number = '{attendance.week_no}';";
-                    MySqlCommand cmnd2 = new MySqlCommand(query4, cnn);
-                    cmnd2.ExecuteNonQuery();
-                    message = "Attendance updated successfully";
-                }
-                else if (a == 1)
-                {
-                    message = "Already taken";
-                }
-                return Ok(new { message = message, a = a });
+                //string query3 = $"SELECT attended FROM attendance WHERE student_id= '{attendance.student_id}' AND schedule_id = '{attendance.schedule_id}' AND week_no = '{attendance.week_no}'";
                 cnn.Close();
+                var message = new { message = "Already taken" };
+                return Ok(message);
             }
         }
     }
